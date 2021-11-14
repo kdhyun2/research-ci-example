f285413f5c5878f6fe59c0c063ed72c7f0bfb9c3
kdhyun2 <ehdgus9233@gmail.com>
2021-11-14 23:04:10 +0900

    fix token

--- main_test.py
+++ main_test.py
@@ -5,7 +5,7 @@ import main
 class MainTest(unittest.TestCase):
     def test_helloworld(self):
         ret = main.helloworld("TEST")
-        self.assertEqual(ret, "Hello World! TEST")
+        self.assertEqual(ret, "Hello World! Denial")
 
 
 if __name__ == "__main__":
